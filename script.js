b2b941ef3e0739eb3a7aa49aa98a488c5137fd22
Federico-Bargosi <bargossifede98@gmail.com>
2024-11-07 16:15:12 +0100

    feat:esercizio completo

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const img1 = document.querySelector(".img");
 const btnElem = document.querySelector(".btn");
 
-btnElem.addEventListener("click" , function() {
-    img1.src = './img/yellow_lamp.png';
+btnElem.addEventListener("click" , function(){
+    if (img1.src.includes('white_lamp.png')){
+        img1.src = './img/yellow_lamp.png';
+        btnElem.innerHTML = ('Spegni');
+    } else {
+        img1.src = './img/white_lamp.png';
+        btnElem.innerHTML = ('Accendi');
+    }
+})
+
+
+
+/*btnElem.addEventListener("click" , function() {
+    img1.src = 'yellow_lamp.png';
 })
     
 
 btnElem.addEventListener("click" , function(){
     btnElem.innerHTML = ("Spegni");
-})
+})*/
