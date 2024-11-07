8c8000fab011d0437471e9bfca2f3b0dd281a1cd
Federico-Bargosi <bargossifede98@gmail.com>
2024-11-07 16:06:26 +0100

    feat:esercizio completo

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 const img1 = document.querySelector(".img");
-const img2 = document.querySelector(".img2");
 const btnElem = document.querySelector(".btn");
-const btnElem2 = document.querySelector(".btn2");
 
 btnElem.addEventListener("click" , function() {
-    img2.classList.add("active");
-    img1.classList.add("none");
+    img1.src = './img/yellow_lamp.png';
 })
+    
 
 btnElem.addEventListener("click" , function(){
-    btnElem2.classList.add("active");
-    btnElem.classList.add("none");
-})
-
-btnElem2.addEventListener("click" , function(){
-    btnElem.classList.remove("none");
-    btnElem2.classList.remove("active");
-    img2.classList.remove("active");
-    img1.classList.remove("none");
+    btnElem.innerHTML = ("Spegni");
 })
